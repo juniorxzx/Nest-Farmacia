cbc2904446eddbc897ab4f5818fc95907aa8dc49
Junior <alexanroju@hotmail.com>
2022-09-13 11:19:40 -0300

    13-09-2022

---
+++ src/produto/controllers/produto.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Produto } from "../entities/produto.entity";
+import { ProdutoService } from "../services/produto.service";
+
+@Controller('/produto')
+export class ProdutoController{
+
+    constructor(private readonly service: ProdutoService){}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Produto[]> {
+        return this.service.findAll()
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
+        return this.service.findById(id)
+    }
+
+    @Get('/descricao/:descricao')
+    @HttpCode(HttpStatus.OK)
+    findByDescricao(@Param('descricao') descricao: string): Promise<Produto[]>{
+        return this.service.findByDescricao(descricao)
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() produto: Produto): Promise<Produto>{
+        return this.service.create(produto)
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)    
+    update(@Body() produto: Produto): Promise<Produto>{
+        return this.service.update(produto)
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.service.delete(id)
+    }
+
+}
